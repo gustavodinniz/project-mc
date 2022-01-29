42e713e45426f64ef6902fe9619d6737cea6d92e
gustavodiniz <gustavo.diniz@tangerino.com.br>
2022-01-29 10:06:24 -0300

    first commit

---
+++ src/main/java/br/com/gustavodiniz/projectmc/ProjectMcApplication.java
@@ -0,0 +1,13 @@
+package br.com.gustavodiniz.projectmc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectMcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectMcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/gustavodiniz/projectmc/ProjectMcApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gustavodiniz.projectmc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectMcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
