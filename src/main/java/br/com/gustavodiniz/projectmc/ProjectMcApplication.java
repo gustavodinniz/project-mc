3c8097a9808919f291521956c04c82035aad10c9
gustavodiniz <gustavo.diniz@tangerino.com.br>
2022-01-29 11:08:26 -0300

    creating instantiation operation

--- src/main/java/br/com/gustavodiniz/projectmc/ProjectMcApplication.java
+++ src/main/java/br/com/gustavodiniz/projectmc/ProjectMcApplication.java
@@ -1,13 +1,30 @@
 package br.com.gustavodiniz.projectmc;
 
+import br.com.gustavodiniz.projectmc.entities.Category;
+import br.com.gustavodiniz.projectmc.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class ProjectMcApplication {
+public class ProjectMcApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProjectMcApplication.class, args);
-	}
+    @Autowired
+    private CategoryRepository categoryRepository;
 
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectMcApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Category cat1 = new Category(null, "Informática");
+        Category cat2 = new Category(null, "Escritório");
+
+        categoryRepository.saveAll(Arrays.asList(cat1, cat2));
+
+    }
 }
