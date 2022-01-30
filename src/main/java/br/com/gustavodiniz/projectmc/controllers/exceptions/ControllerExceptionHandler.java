1c438e2071ede7aec5627e3fbcc5677dc6295ed6
gustavodiniz <gustavo.diniz@tangerino.com.br>
2022-01-30 09:53:06 -0300

    customizing the validation return

--- src/main/java/br/com/gustavodiniz/projectmc/controllers/exceptions/ControllerExceptionHandler.java
+++ src/main/java/br/com/gustavodiniz/projectmc/controllers/exceptions/ControllerExceptionHandler.java
@@ -4,6 +4,8 @@ import br.com.gustavodiniz.projectmc.services.exceptions.DataIntegrityException;
 import br.com.gustavodiniz.projectmc.services.exceptions.ObjectNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -21,6 +23,15 @@ public class ControllerExceptionHandler {
     @ExceptionHandler(DataIntegrityException.class)
     public ResponseEntity<StandardError> dataIntegrity(DataIntegrityException exception, HttpServletRequest request) {
         StandardError error = new StandardError(HttpStatus.BAD_REQUEST.value(), exception.getMessage(), System.currentTimeMillis());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<StandardError> validation(MethodArgumentNotValidException exception, HttpServletRequest request) {
+        ValidationError error = new ValidationError(HttpStatus.BAD_REQUEST.value(), "Validation error", System.currentTimeMillis());
+        for (FieldError x : exception.getBindingResult().getFieldErrors()) {
+            error.addError(x.getField(), x.getDefaultMessage());
+        }
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
     }
 }
