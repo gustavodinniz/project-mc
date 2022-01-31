aeab9c7b57003def54436d4a1248143795731b57
gustavodiniz <gustavo.diniz@tangerino.com.br>
2022-01-30 23:19:11 -0300

    documentation with swagger part two

--- src/main/java/br/com/gustavodiniz/projectmc/config/SwaggerConfig.java
+++ src/main/java/br/com/gustavodiniz/projectmc/config/SwaggerConfig.java
@@ -6,9 +6,13 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
 
 @Configuration
 @EnableSwagger2
@@ -24,8 +28,22 @@ public class SwaggerConfig implements WebMvcConfigurer {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("br.com.gustavodiniz.projectmc.controllers"))
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "Spring Boot Course API",
+                "This API is used in the Spring Boot course",
+                "Version 1.0",
+                "",
+                new Contact("Gustavo Diniz", "https://github.com/gustavodinniz", "gustavodinniz@hotmail.com"),
+                "Allowed use for students",
+                "",
+                Collections.emptyList() // Vendor Extensions
+        );
     }
 }
